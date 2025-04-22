4be751e43a3ca9c0477765fba69126bcace963ba
SundayYogurt <kritsanalalu5@gmail.com>
2025-04-23 00:00:05 +0700

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/2567-portfolio",
+  base: "/PortfolioAndCV",
 });
